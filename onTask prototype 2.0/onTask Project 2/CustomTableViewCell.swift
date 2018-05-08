94b9a66c812e3cdc416571bf6b1d3d1297aa198f
William Jeong <wjeong@umass.edu>
2018-05-07 20:22:20 -0400

    General UI with Static bottom functions

---
+++ onTask prototype 2.0/onTask Project 2/CustomTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  CustomTableViewCell.swift
+//  onTask Project 2
+//
+//  Created by Will Jeong on 5/4/18.
+//  Copyright © 2018 Jackie Wang. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    
+    @IBOutlet weak var choreImage: UIImageView!
+    
+    @IBOutlet weak var checkButton: UIButton!
+    
+    var checked = false;
+    let checkMark = UIImage(named: "checked box") as UIImage!
+    let uncheckMark = UIImage(named: "unchecked box") as UIImage!
+    
+    var editting = false
+    @IBAction func edditingFunction(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func checkButton(_ sender: UIButton) {
+        if editting == false{
+            checked = !checked
+            if checked{
+                sender.setImage(checkMark, for: .normal)
+                //sender.setBackgroundImage(checkMark, for: .normal)
+                
+            }
+            else{
+                sender.setImage(uncheckMark, for: .normal)
+                //sender.setBackgroundImage(uncheckMark, for: .normal)
+                
+            }
+        }
+        
+    }
+    @IBOutlet weak var choreLabel: UILabel!
+    
+    @IBOutlet weak var userType: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
--- onTask prototype 2.0/onTask Project 2/ViewController.swift
+++ onTask prototype 2.0/onTask Project 2/ViewController.swift
@@ -8,42 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var taskNameLabel: UIButton!
-
+    let elements = ["broom", "cook", "dishes", "fold laundry", "litterbox", "make bed", "mop floor", "toilet", "trash"]
+    let users = ["UserA", "UserB", "UserC"]
+    
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var date: UILabel!
     
-    @IBAction func checkBoxTouched(_ sender: UIButton) {
-        let checkMark = UIImage(named: "checkmark") as UIImage!
-        
-        if sender.backgroundImage(for: .normal) != nil{
-            sender.setBackgroundImage(nil, for: .normal)
-            
+    var finished = false
+    
+    @IBAction func addRemove(_ sender: UIButton) {
+        finished = !finished
+        if finished{
+            sender.setImage(UIImage(named: "done button 60x60"), for: .normal)
+
         }
         else{
-            sender.setBackgroundImage(checkMark, for: .normal)
-            
+            sender.setImage(UIImage(named: "addandedit.png"), for: .normal)
+
         }
-        
-        
     }
     
+    
     override func viewDidLoad() {
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         let currDate = Date()
-        self.date.text = currDate.toString(dateFormat: "dd-MMM-yyyy")
+        self.date.text = currDate.toString(dateFormat: "MMM-dd")
+        
+        
+
 
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return elements.count
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
     
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
+        cell.choreLabel.text = elements[indexPath.row]
+        cell.choreImage.image = UIImage(named: elements[indexPath.row])
+        cell.userType.text = users[Int(arc4random_uniform(3))]
+        return cell
+    }
     
     
     
@@ -58,6 +76,6 @@ extension Date
         dateFormatter.dateFormat = format
         return dateFormatter.string(from: self)
     }
-    
+
 }
 
